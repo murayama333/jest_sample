b55c6f94d218ad02b84a63227a4c06edfee2f1b2
murayama-d <murayama@code-d.jp>
2019-03-08 14:30:11 +0900

    update5

--- test/my-lib.test.js
+++ test/my-lib.test.js
@@ -3,9 +3,9 @@ test('counter 1 -> 2', () => {
   expect(myLib.counter(1)).toBe(2)
 });
 
-// test('add 1, 2 -> 3', () => {
-//   expect(myLib.add(1, 2)).toBe(3)
-// });
+test('add 1, 2 -> 3', () => {
+  expect(myLib.add(1, 2)).toBe(3)
+});
 
 // test('divide 1, 2 -> -1', () => {
 //   expect(myLib.divide(1, 2)).toBe(-1)
